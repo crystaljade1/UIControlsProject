b77f9c1b5e9a595fc198badd434b636e829429d0
Crystal Jade Allen-Washington <crystaljade@macbooks-air.lan>
2017-05-04 16:30:20 -0400

    Initial Commit

---
+++ UIControlsProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIControlsProject
+//
+//  Created by Crystal Jade Allen-Washington on 5/4/17.
+//  Copyright © 2017 Crystal Jade Allen-Washington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
